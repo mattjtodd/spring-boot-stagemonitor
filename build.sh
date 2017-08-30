57ba98f1d43acf06986c355d8a21c1ab5e2df41e
Matt Todd <matt.todd@resonate.tech>
2017-08-30 14:53:20 +0100

    Added build and run

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker build -t mattjtodd/spring-boot-stagemonitor .
+exec docker build -t mattjtodd/spring-boot-stagemonitor .
---
+++ run.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+exec docker run -p 8080:8080 -it mattjtodd/spring-boot-stagemonitor
